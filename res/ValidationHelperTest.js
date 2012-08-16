8b669361751356a367bba50bed87ade63174489b
Riz <riz247@gmail.com>
2012-08-16 21:29:15 +0100

    fixed broken text

--- res/ValidationHelperTest.js
+++ res/ValidationHelperTest.js
@@ -2,61 +2,59 @@ function runValidationHelperTests(){
   module("Validation Helper Tests");
 
   test("Test empty validation", function() {
-    var obj = new ValidationHelper($("#dummy"), "");
+    var obj = new ValidationHelper($("<input type='hidden' value='' />"));
     obj.isRequired();
     equal(false, obj.isValid(), "Should not be valid");
 
-    var obj = new ValidationHelper($("#dummy"), "ee");
+    var obj = new ValidationHelper($("<input type='hidden' value='ee'/>"));
     obj.isRequired();
     equal(true, obj.isValid(), "Should be valid");
   });
 
   test("Test digit validation", function() {
-    var obj = new ValidationHelper($("#dummy"), "5");
+    var obj = new ValidationHelper($("<input type='hidden' value='5' />"));
     obj.isDigit();
     equal(true, obj.isValid(), "Should be valid");
 
-    var obj = new ValidationHelper($("#dummy"), "e");
+    var obj = new ValidationHelper($("<input type='hidden' value='e' />"));
     obj.isDigit();
     equal(false, obj.isValid(), "Should be invalid");
 
-    var obj = new ValidationHelper($("#dummy"), "e");
-    obj.isDigit();
-    equal(false, obj.isValid(), "Should be invalid");
   });
 
+
   test("Test in range validation", function() {
-    var obj = new ValidationHelper($("#dummy"), "5");
+    var obj = new ValidationHelper($("<input type='hidden' value='5' />"));
     obj.inRange(5, 10);
     equal(true, obj.isValid(), "Should be valid");
 
-    var obj = new ValidationHelper($("#dummy"), "10");
+    var obj = new ValidationHelper($("<input type='hidden' value='10' />"));
     obj.inRange(5, 10);
     equal(true, obj.isValid(), "Should be valid");
 
-    var obj = new ValidationHelper($("#dummy"), "11");
+    var obj = new ValidationHelper($("<input type='hidden' value='11' />"));
     obj.inRange(5, 10);
     equal(false, obj.isValid(), "Should be invalid");
 
-    var obj = new ValidationHelper($("#dummy"), "4");
+    var obj = new ValidationHelper($("<input type='hidden' value='4' />"));
     obj.inRange(5, 10);
     equal(false, obj.isValid(), "Should be invalid");
 
-    var obj = new ValidationHelper($("#dummy"), "5.7");
+    var obj = new ValidationHelper($("<input type='hidden' value='5.7' />"));
     obj.inRange(5, 10);
     equal(true, obj.isValid(), "Should be invalid");
 
   });
 
   test("Test combination of validation", function(){
-    var obj = new ValidationHelper($("#dummy"), '5');
+    var obj = new ValidationHelper($("<input type='hidden' value='5' />"));
     obj.isRequired().isDigit();
     equal(true, obj.isValid(), "Should be valid");
   });
 
 
   test("Validation test greater than", function(){
-    var obj = new ValidationHelper($("#dummy"), '5');
+    var obj = new ValidationHelper($("<input type='hidden' value='5' />"));
     obj.isGreaterThan(4);
     equal(true, obj.isValid(), "Should be valid");
 
@@ -65,7 +63,7 @@ function runValidationHelperTests(){
   });
 
   test("Validation test less than", function(){
-    var obj = new ValidationHelper($("#dummy"), '5');
+    var obj = new ValidationHelper($("<input type='hidden' value='5' />"));
     obj.isLessThan(6);
     equal(true, obj.isValid(), "Should be valid");
 
@@ -74,73 +72,73 @@ function runValidationHelperTests(){
   });
 
   test("Validation test decimal", function(){
-    var obj = new ValidationHelper($("#dummy"), '5.6');
+    var obj = new ValidationHelper($("<input type='hidden' value='5.6' />"));
     obj.isDecimal();
     equal(true, obj.isValid(), "Should be valid");
 
-    var obj = new ValidationHelper($("#dummy"), '56');
+    var obj = new ValidationHelper($("<input type='hidden' value='56' />"));
     obj.isDecimal();
     equal(true, obj.isValid(), "Should be valid");
 
   });
 
   test("Validation test date", function(){
-    var obj = new ValidationHelper($("#dummy"), '01/01/2001');
+    var obj = new ValidationHelper($("<input type='hidden' value='01/01/2001' />"));
     obj.isDate();
     equal(true, obj.isValid(), "Should be valid");
-    var obj = new ValidationHelper($("#dummy"), '01/01/01');
+    var obj = new ValidationHelper($("<input type='hidden' value='01/01/01' />"));
     obj.isDate();
     equal(false, obj.isValid(), "Should be valid");
   });
 
-  test("Test conf values set", function(){
-    var obj = new ValidationHelper("","", {continueOnInvalid: false});
-    equal(obj.mConf.continueOnInvalid, false, "should be set to false");
 
-    var obj = new ValidationHelper("","", {validateEmpty: true});
-    equal(obj.mConf.validateEmpty, true, "should be set to true");
-  })
 
   test("Precheck continue on invalid check", function(){
-    var obj = new ValidationHelper("", "ff");
+    var obj = new ValidationHelper($("<input type='hidden' value='ff' />"));
     obj.mIsValid = false;
     equal(obj.preCheck(), true, "Should return true");
 
-    var obj = new ValidationHelper("","ff", {continueOnInvalid: false});
+    var obj = new ValidationHelper($("<input type='hidden' value='ff' />"));
+
+    obj.setContinueOnInvalid(false);
     obj.mIsValid = false;
     equal(obj.preCheck(), false, "Should return false");
   });
 
   test("Precheck continue on empty value", function(){
-    var obj = new ValidationHelper("", "");
+    var obj = new ValidationHelper($("<input type='hidden' value='' />"))
+                  .setValidateEmpty(false);
     equal(obj.preCheck(), false, "Should return false");
 
-    var obj = new ValidationHelper("","", {validateEmpty: true});
+    var obj = new ValidationHelper($("<input type='hidden' value='' />"))
+                  .setValidateEmpty(true);
+
     equal(obj.preCheck(), true, "Should return true");
   });
 
   test("Should call precheck method", function(){
-    var obj = new ValidationHelper("","", {validateEmpty: true});
+    var obj = new ValidationHelper($("<input type='hidden' value='' />"));
     this.spy(obj, "preCheck");
     obj.isRequired();
     ok(obj.preCheck.calledOnce);
   });
 
   test("test isValidRemote method precheck method", function(){
-    var obj = new ValidationHelper($("dummy"),"123");
+    var obj = new ValidationHelper($("<input type='hidden' value='123' />"));
     obj.isValidRemote("testData/validResponse.html", {some: "data"}).end();
-    equal(obj.isValid(), true, "Should is valid");
+    equal(obj.isValid(), true, "is valid");
 
-    var obj = new ValidationHelper($("dummy"),"123");
+    var obj = new ValidationHelper($("<input type='hidden' value='123' />"));
     obj.isValidRemote("testData/inValidResponse.html", {some: "data"}).end();
     equal(obj.isValid(), false, "Should be invalid");
   });
+
 
   test("test error label placed in specified container", function(){
     //setup
     $("body").append("<div id='errContainer'><div>");
 
-    var obj = new ValidationHelper($("#dummy"), "", {target: $("#errContainer")});
+    var obj = new ValidationHelper($("<input type='hidden' value=''>")).setTarget($("#errContainer"))
     obj.isRequired();
     if($("#errContainer .error").length >0){
       ok(true, "Error label placed in target element");
@@ -152,5 +150,7 @@ function runValidationHelperTests(){
     //teardown
     $("#errContainer").remove();
   })
+
+
 
 }
